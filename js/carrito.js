8265b7dd716dcf01402fca99a985134919ff23a3
Gustavo Olmos <grolmos159@gmail.com>
2023-08-25 14:27:21 -0300

    correccion de preentrega3

--- js/carrito.js
+++ js/carrito.js
@@ -1,34 +1,95 @@
 /* carrito de compras */
 
-let carrito = [];//es un array vacio donde se van a cargar los objetos(productos) que introduzcan los usuarios.
+let carrito = []; //es un array vacio donde se van a cargar los objetos(productos) que introduzcan los usuarios.
 
-function agregarAlCarrito(){//es una funcion que va a agregar los productos que soliciten los usuarios.
-    const producto = document.getElementById("producto").value//es una constante que me toma el valor de el input "producto" en el formulario.
-    const cantidad = parseInt(document.getElementById("cantidad").value)//es una constante que me toma el valor de el input "cantidad" en el formulario. el ParseInt vuelve la cantidad, que es un string, a numero entero.
-    if(!isNaN(cantidad) && cantidad > 0){//(IsNaN: significa no es un numero, !isNaN: significa es un numero) si cantidad es un numero y ademas es mayor a 0.
-        const item = {producto, cantidad};//creo una constante que cree un objeto de dos valores: producto y cantidad. estos valores salen de las constantes anteriores.
+function agregarAlCarrito() {
+  const producto = document.getElementById("producto").value; 
+  
+  const cantidad = parseInt(document.getElementById("cantidad").value);
+  const mensajeAlert = document.getElementById("carrito");
+  mensajeAlert.innerHTML = "";
 
-        carrito.push(item)//va a agregar al array del carrito los datos que ingrese el usuario.
+  let mensaje = document.createElement("h1");
+  mensajeAlert.appendChild(mensaje);
 
-        actualizarCarrito()//si todo es correcto, que actualice el carrito, que es la funcion que esta mas abajo.
-    }else{
-        alert("Por favor ingresa una cantidad valida")
-    }
+
+
+  if (!isNaN(cantidad) && cantidad > 0) {
+    const item = { producto, cantidad };
+
+    
+
+    carrito.push(item);
+    
+    actualizarCarrito(); 
+    
+  } else {
+    mensaje.innerText = "✏ Por favor ingresa productos 🙋‍♂️";//cambio un alert por un mensaje en el html. todos mis mensajes de alerta se van a ejecutar dentro del div del carrito.
+  }
 }
 
-function guardarCarrito(){//esta funcion guarda el carrito, una vez completado, al localStorage. para poder guardarlo tiene que pasar los numeros a strings con stringify!
-    const carritoJSON = JSON.stringify(carrito);//creo una constante que me va a crear un string del carrito paa poder guardarlo en el localStorage.
-    localStorage.setItem("carritoData", carritoJSON)//indicamos que almacene en el localStorage con esa clave y valor que 
-    alert("Carrito guardado")//nos avisa que ya se guardo.
+//Corrección:
+// 2- Cuando guardes los productos en el carrito, fijate de tener una validación para no guardar un carrito vacío, porque si no agrego nada y le doy a guardar, me guarda un array vacío en el localStorage.
+
+//creo condicional que evalua si hay algo en el carrito y si no hay nada agregado envia un mensaje que hay que agregar. si hay elementos envia un mje que guarda el carrito.
+
+function guardarCarrito() {
+
+  let mensajeAlert = document.getElementById("carrito");
+  mensajeAlert.innerHTML = "";
+
+  let mensaje = document.createElement("h1");
+  mensajeAlert.appendChild(mensaje);
+
+  if (carrito.length > 0) {
+    const carritoJSON = JSON.stringify(carrito); //creo una constante que me va a crear un string del carrito paa poder guardarlo en el localStorage.
+    localStorage.setItem("carritoData", carritoJSON); //indicamos que almacene en el localStorage con esa clave y valor que
+    mensaje.innerText = "🛒 Carrito guardado 🛒"; //nos avisa que ya se guardo.
+  } else {
+    mensaje.innerText = "❌ Para guardar tu carrito debes agregar productos";
+  }
 }
 
-function actualizarCarrito(){//va cargando los productos en la lista del html
-    const carritoElement = document.getElementById("carrito")//constante que toma el carrito desde el ul del html
-    carritoElement.innerHTML="";//agrega al html lo siguiente:
+function actualizarCarrito() {
+  //va cargando los productos en la lista del html
+  const carritoElement = document.getElementById("carrito"); //constante que toma el carrito desde el ul del html
+  carritoElement.innerHTML = ""; //agrega al html lo siguiente:
 
-    carrito.forEach((item)=>{//por cada elemento que encuentre en el array carrito, que inicialmente estaba vacio, que cree una li en el ul del html asi lo muestra en el dom.
-        const listItem = document.createElement("li")//es una constante que crea las li.
-        listItem.textContent = `${item.producto} - cantidad:  ${item.cantidad}`//muestra un texto con el valor de producto y el valor de la cantidad.(- cantidad es un string)
-        carritoElement.appendChild(listItem)//Es hijo del innerHTML y esta indicando que alli se va a ejecutar el ListItem.
-    })
+  carrito.forEach((item) => {
+    //por cada elemento que encuentre en el array carrito, que inicialmente estaba vacio, que cree una li en el ul del html asi lo muestra en el dom.
+    
+
+    const listItem = document.createElement("li"); //es una constante que crea las li.
+    listItem.textContent = `${item.producto} - cantidad:  ${item.cantidad}`; //muestra un texto con el valor de producto y el valor de la cantidad.(- cantidad es un string)
+    carritoElement.appendChild(listItem); //Es hijo del innerHTML y esta indicando que alli se va a ejecutar el ListItem.
+  });
 }
+
+// 3- Agrega una opción para obtener los datos que guardaste en el localStorage, para poder verlos en pantalla.
+
+//creo funcion que me muestra lo que agregue al carrito en una lista y si no agrego nada muestra un mje de que no hay nada.
+function verCarrito(){
+    const carritoJSON = localStorage.getItem("carritoData");
+    const contenidoDiv = document.getElementById("carrito");
+
+    contenidoDiv.innerHTML = ""; 
+
+    if (carritoJSON) {
+        const carrito = JSON.parse(carritoJSON);
+        const itemTexto = document.createElement("h2");
+        itemTexto.textContent = `Tu carrito tiene guardado:`
+        contenidoDiv.appendChild(itemTexto);
+        carrito.forEach((item) => {
+          const itemTexto = document.createElement("p");
+          itemTexto.textContent = `${item.producto} - Cantidad: ${item.cantidad}`;
+          contenidoDiv.appendChild(itemTexto);
+        });
+      } else {
+        const itemTexto = document.createElement("h2");
+        itemTexto.textContent = `❌ No tenés carrito guardado.`
+        contenidoDiv.appendChild(itemTexto);        
+      }
+}
+
+
+    
--- js/login.js
+++ js/login.js
@@ -1,40 +1,30 @@
+/* profe: simplifique el login para poder realizarlo con el DOM:
 
-/* Mi proyecto es un market place para una empresa de venta de aparatologia medica.
+1- Ya no se puede utilizar el prompt ni el alert, así que utiliza el DOM para capturar los datos del usuario.
+*/
 
-Esta seccion es para que el usuario se loguee o se registre.
-1ro: Creo una funcion que verifique si el nombre de usuario esta registrado. En el caso de que no este registrado doy la opcion de registrarse. */
+//primero creo un evento que no permita que se recargue la pagina al presionar el boton de enviar.
+document.addEventListener("DOMContentLoaded", function () {
+  const loginForm = document.getElementById("login-form");
 
-function verificarUsuarioEnLista(nombre, listaNombres) {
-    return listaNombres.some((nombreLista) => nombreLista === nombre);// uso el metodo some para encuntrar en nombre en la lista de usuarios. me recorre toda la lista. quiero que respete mayusculas, minusculas y numeros!
-}
+  loginForm.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-function buscarNombreEnLista() {
-    const nombres = ['Ana22', 'Juan2000', 'MaríaFlia', 'Pedro09', 'Luis65'];
+    //creo constantes para captar los datos ingresados por el usuario y que voy a utilizar en mi funcion.
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
 
-    // Le pido al usuario que ingrese su nombre de usuario
-    const nombreUsuario = prompt('Ingresa tu nombre de usuario respetando mayusculas y numeros');
+    let mensajeBox = document.getElementById("error-message");
+    mensajeBox.innerHTML = "";
 
-    // Verifico si el usuario esta en la lista
-    const estaEnLaLista = verificarUsuarioEnLista(nombreUsuario, nombres);
+    let mensaje = document.createElement("h1");
+    mensajeBox.appendChild(mensaje);
 
-    // Si el usuario esta en la lista doy saludo de bienvenida
-    if (estaEnLaLista) {
-        alert(` 🙋‍♂️ ¡Bienvenid@ ${nombreUsuario}!🙋‍♀️ `);
-    } else { // Si el usuario no esta en la lista le doy aviso y le ofrezco registrarse
-        alert(`💥 El nombre "${nombreUsuario}" no corresponde a un usuario registrado. Hace click en ACEPTAR y registrate. 💥`);
-
-        const nombreAgregar = prompt("Crea tu nombre de usuario. Recorda que podes usar mayusculas y numeros tambien"); //le pido que ingrese el nombre de usuario
-
-        nombres.push(nombreAgregar); //lo agrega a la lista
-
-        alert(`El nombre "${nombreAgregar}" de usuario ya se  agrego a nuestra lista de usuarios`); //doy un alerta que ya se agrego el usuario a la lista
-
-        console.log(nombres); //muestra la lista de usuarios en la consola
+    // creo condicional que va a validar los datos y si son correctos imprime un mensaje de bienvenida y si no uno de error
+    if (username === "Gusta37" && password === "1234") {
+      mensaje.innerText = "Bienvenido Gustavo 🙋‍♂️!";
+    } else {
+      mensaje.innerText = " 🤨 Datos incorrectos 😒 ";
     }
-}
-
-// Llamo a la función para que comience la búsqueda
-buscarNombreEnLista();
-
-
-
+  });
+});
